bacc0756a0fb76cd24795e98398f2b1d0d0a0571
Gergely Pali <pali.janos.gergely@gmail.com>
2022-11-26 16:24:57 +0100

    my solution for the functions; game can be played to completion

--- basketball.js
+++ basketball.js
@@ -24,12 +24,13 @@ let timer = document.getElementById("timer");
 
 // Create a template for a circle
 // One instance will be the ball, other will be the hoop
-function Circle(color_, fill_, x_, y_, dx_, dy_, radius_) {
+function Circle(color_, fill_, x_, y_, dx_, dy_, radius_, isBall_) {
   this.x = x_;  // position along x axis
   this.y = y_;  // position along y axis
   this.dx = dx_;  // amount to change x on each redraw of the canvas
   this.dy = dy_;  // amount to change y on each redraw of the canvas
   this.radius = radius_;
+  this.isBall = isBall_;
 
   // member function to draw the circle
   // https://www.w3schools.com/html/html5_canvas.asp
@@ -46,13 +47,35 @@ function Circle(color_, fill_, x_, y_, dx_, dy_, radius_) {
   // member function to make the circle move
   this.update = function (deltaTime_) {
     // we have some work to do here
-
+    this.x += this.dx
+    this.y += this.dy
+    // left and right border checking and bounce back the circle
+    if (this.x + this.radius > canvas.width || this.x < this.radius)
+    {
+      this.dx = -this.dx
+    }
+    // floor checking
+    if (this.y >=  canvas.height - this.radius)
+    {
+      this.y = canvas.height - this.radius
+      // add some mechanical energy loss
+      this.dy = -this.dy * 0.6
+    }
+    // gravity only to the ball
+    if (this.isBall)
+    {
+      this.dy += 0.2
+    }
   };
   
   // member function to detect ball going through the hoop
   this.collide = function (other_) {
     // we have some work to do here
-    
+    if (this.y - this.radius > other_.y - other_.radius * 0.85 && this.y + this.radius < other_.y + other_.radius * 0.85
+     && this.x - this.radius > other_.x - other_.radius * 0.85 && this.x + this.radius < other_.x + other_.radius * 0.85)
+    {
+      return true
+    }
     return false;
   };
 }
@@ -84,14 +107,21 @@ let hoop = new Circle(
 
 function shoot(event) {
   // we have some work to do here
+  if(basketball.dy < 0.2)
+  {
+    basketball.dx = 0
+    basketball.dy = -9
+  }
 }
 
 function left(event) {
   // we have some work to do here
+  basketball.dx = -2.5
 }
 
 function right(event) {
   // we have some work to do here
+  basketball.dx = 2.5
 }
 
 
@@ -108,6 +138,9 @@ function gameover() {
   winnerElement.style.visibility = "visible";
   // we have some work to do here
   //   to hide the buttons that move the ball
+  my_left_button.style.visibility = "hidden"
+  my_shoot_button.style.visibility = "hidden"
+  my_right_button.style.visibility = "hidden"
 }
 
 
@@ -130,6 +163,9 @@ function gameLoop( nowTime, lastTime ) {
   if ( basketball.collide(hoop) )
   {
     score++;
+    // reset the basketball to the ground in case of a bucket
+    basketball.y = canvas.height - basketball.radius
+    basketball.dy = 0
     // Update the score value in the HTML element
     // (remember, that element is outside the canvas)
     scoreElement.innerHTML = score;
@@ -141,7 +177,7 @@ function gameLoop( nowTime, lastTime ) {
     }
   }
 
-  if ( score >= winningScore )
+  if ( score >= winningScore)
   {
     // then stop animating
     timer.innerHTML = (lastTime/1000).toFixed(2);
